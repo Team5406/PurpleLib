695a4bf7266386f6ceae6c3e223d70273d76001d
ChaiJustin <maramotchai@gmail.com>
2024-03-29 15:30:07 -0400

    navxUSB (Does not work)

--- hardware/kauailabs/NavX2.java
+++ hardware/kauailabs/NavX2.java
@@ -20,6 +20,7 @@ import edu.wpi.first.units.Measure;
 import edu.wpi.first.units.Units;
 import edu.wpi.first.units.Velocity;
 import edu.wpi.first.wpilibj.SPI;
+import edu.wpi.first.wpilibj.SerialPort;
 
 /** NavX2 */
 public class NavX2 implements LoggableHardware, AutoCloseable {
@@ -63,7 +64,7 @@ public class NavX2 implements LoggableHardware, AutoCloseable {
    */
   public NavX2(ID id, int updateRate) {
     this.m_name = id.name;
-    this.m_navx = new AHRS(SPI.Port.kMXP, (byte)updateRate);
+    this.m_navx = new AHRS(SerialPort.Port.kUSB2);
     this.m_inputs = new NavX2InputsAutoLogged();
     this.m_simNavXYaw = new SimDouble(SimDeviceDataJNI.getSimValueHandle(SimDeviceDataJNI.getSimDeviceHandle("navX-Sensor[0]"), "Yaw"));
     System.out.println();
